cd6507e09afd61b0ffb773842330a5afa79ed079
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-03-17 19:38:28 +0100

    Solution to lock box problem

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Contains the function that checks if the boxes in a box can be unlocked or not
+"""
+
+
+def checkbox(boxes, keys, index, checked):
+    """
+    This function checks if the box can be unlocked
+    Args:
+      box (list): The box to be checked
+      keys (list): A list of keys available to unlock the box
+    Return:
+      True, if box can be unlocked otherwise False
+    """
+
+    for box in range(1, len(boxes)):
+        if box in checked:
+            continue
+        else:
+            if boxes[box]:
+                for key in boxes[box]:
+                    if key in keys:
+                        return [True, box]
+            else:
+                if index in keys:
+                    return [True, box]
+    return [False, box]
+
+
+def canUnlockAll(boxes):
+    """
+    This function checks if all the boxes can be unlocked
+    Args:
+      boxes (list): A list of boxes to be checked
+    Return:
+      True, if all boxes can be unlocked otherwise false
+    """
+
+    keys = []
+    unlocked = [0]
+
+    for key in boxes[0]:
+        keys.append(key)
+
+    for box in range(1, len(boxes)):
+        unlock = checkbox(boxes, keys, box, unlocked)
+        if unlock[0] is True:
+            for key in boxes[box]:
+                keys.append(key)
+                unlocked.append(unlock[1])
+
+    if len(unlocked) == len(boxes):
+        return True
+    else:
+        return False
